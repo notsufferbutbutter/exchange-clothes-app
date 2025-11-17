6c285b260c8b31b6731d38c3771f9bd7086178e8
Linh Pham <linh.pham@oldendorff.com>
2025-11-17 21:20:13 +0100

    feat(login): add logic for signup

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,48 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { provideIcons } from '@ng-icons/core';
 import { lucideCheck, lucideChevronDown } from '@ng-icons/lucide';
 import { HlmButtonImports } from '@spartan-ng/helm/button';
 import { HlmCardImports } from '@spartan-ng/helm/card';
 import { HlmInputImports } from '@spartan-ng/helm/input';
 import { HlmLabelImports } from '@spartan-ng/helm/label';
-import { RouterLink } from "@angular/router";
+import { RouterLink } from '@angular/router';
+import { FormBuilder, ReactiveFormsModule, Validators } from "@angular/forms";
+import { AuthService } from 'src/app/services/auth.service';
+import { HlmError } from '@spartan-ng/helm/form-field';
+import { passwordCannotContainsEmailNameValidation } from 'src/app/libs/validators/passwordCannotContainEmailNameValidation';
+import { passwordStrengthValidation } from 'src/app/libs/validators/passwordStrengthValidation';
+
 
 @Component({
 	selector: 'app-signup',
-	imports: [HlmCardImports, HlmLabelImports, HlmInputImports, HlmButtonImports, RouterLink],
+	imports: [HlmCardImports, HlmLabelImports, HlmInputImports, HlmButtonImports, RouterLink, ReactiveFormsModule, HlmError],
 	providers: [provideIcons({ lucideCheck, lucideChevronDown })],
 	host: {
 		class: 'contents',
 	},
-	template: `
-		<section class="w-full max-w-sm" hlmCard>
-			<div hlmCardHeader>
-				<h3 hlmCardTitle>Create an account</h3>
-			</div>
-
-			<div hlmCardContent>
-				<form>
-					<div class="flex flex-col gap-6">
-						<div class="grid gap-2">
-							<label hlmLabel for="email">Email</label>
-							<input type="email" id="email" placeholder="m@example.com" required hlmInput />
-						</div>
-
-						<div class="grid gap-2">
-							<div class="flex items-center">
-								<label hlmLabel for="password">Password</label>
-							</div>
-							<input type="password" id="password" hlmInput />
-						</div>
-					</div>
-				</form>
-			</div>
-
-			<div hlmCardFooter class="flex-col gap-2">
-				<button hlmBtn type="submit" class="w-full">Create an account</button>
-        <a routerLink="/login" class="text-base underline-offset-4 hover:underline">Already have an account?</a>
-			</div>
-		</section>
-	`,
+	templateUrl: './signup.component.html'
 })
-export class SignupComponent {}
+export class SignupComponent {
+	private readonly _fb = inject(FormBuilder);
+	private readonly _authService = inject(AuthService);
+
+	errorMessage = signal<string|null>(null);
+	succeededMessage = signal<string|null>(null);
+
+	public signupForm = this._fb.group(
+		{
+			email: ['', 
+				[
+					Validators.required,
+					Validators.email
+				]
+			],
+			password: ['', 
+				[
+					Validators.required,
+					passwordStrengthValidation()
+				]
+			],
+		},
+		{ validators: passwordCannotContainsEmailNameValidation() }
+	)
+
+	async onSubmit() {
+		if (this.signupForm.invalid) return;
+
+		const { email, password } = this.signupForm.value;
+
+		const { error, message } = await this._authService.signupNewUser(email!, password!);
+
+		if ( error ) {
+			this.errorMessage.set(error.message);
+		}
+		else {
+			this.succeededMessage.set(message);
+		}
+	}
+
+	get password() {
+		return this.signupForm.controls['password'];
+	}
+
+	get email() {
+		return this.signupForm.controls['email'];
+	}
+}
