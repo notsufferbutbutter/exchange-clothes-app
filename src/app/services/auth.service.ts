486f0292004002fd912c1771ac6e53e35011a4bf
Linh Pham <linh.pham@oldendorff.com>
2025-11-17 21:21:51 +0100

    feat(auth): use supabase authentication for login and signup

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,16 +1,53 @@
 import { Injectable, signal } from '@angular/core';
+import { supabase } from 'src/backend/infrastructure/supabase/superbase-client';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   readonly isLoggedIn = signal<boolean>(false);
+  readonly user = signal<any>(null);
 
   constructor() {
-
+    this.restoreSession()
   }
 
-  login() { this.isLoggedIn.set(true); }
+  async restoreSession() {
+    const { data } = await supabase.auth.getSession();
+    if ( data?.session ) {
+      this.isLoggedIn.set(true);
+      this.user.set(data.session);
+    }
+  }
 
-  logout() { this.isLoggedIn.set(false); }
+  async signupNewUser(email: string, password: string) {
+    const { data, error } = await supabase.auth.signUp({
+      email: email,
+      password: password
+    })
+
+    if (error) return { error };
+
+    return { message: 'Check your email to verify your account' };
+  }
+
+  async loginWithEmail(email: string, password: string) { 
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email: email,
+      password: password
+    })
+
+    if (error) return { error };
+
+    this.isLoggedIn.set(true);
+    this.user.set(data.user);
+
+    return { data };
+  }
+
+  async logout() { 
+    await supabase.auth.signOut();
+    this.isLoggedIn.set(false); 
+    this.user.set(null);
+  }
 }
