c4f4ff9a5373c014fb95169262b360548359773e
Linh Pham <linh.pham@oldendorff.com>
2025-11-17 21:22:11 +0100

    feat(signup): add signup endpoint

--- src/app/pages/signup.page.ts
+++ src/app/pages/signup.page.ts
@@ -4,6 +4,9 @@ import { SignupComponent } from "../components/signup/signup.component";
 @Component({
     selector: 'app-signup-page',
     imports: [SignupComponent],
+    host: {
+		class: 'w-full max-w-md',
+	},
     template: `
         <div class="flex justify-center items-center min-h-screen pb-20">
             <app-signup></app-signup>
